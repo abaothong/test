f5776b16a9298197003bf0823ad0c08203eaec63
Thong Hao Yi <hy.thong@elken.com>
2020-02-18 22:09:01 +0800

    initial commit

---
+++ Boost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boost
+//
+//  Created by Thong Hao Yi on 18/02/2020.
+//  Copyright © 2020 Abao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
